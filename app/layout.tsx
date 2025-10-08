ab27ed12e0fada2d0fd7ddc3bc7dad3774029343
suleymanovemin <suleymanov.emin2001@gmail.com>
2025-10-08 11:02:47 +0400

    container

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Header />
-        {children}
+        <div className="container mx-auto">{children}</div>
       </body>
     </html>
   );
